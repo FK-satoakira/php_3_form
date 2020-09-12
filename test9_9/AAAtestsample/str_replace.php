2593ef77ee2f0523f8baceda43904a49d7aacfb3
FK-satoakira <69459763+FK-satoakira@users.noreply.github.com>
2020-09-12 14:59:31 +0900

    Add files via upload

---
+++ test9_9/AAAtestsample/str_replace.php
@@ -0,0 +1,10 @@
+<?php
+
+$str=array("英語","数学");
+print_r($str);
+echo "<br>";
+$str=str_replace("英語","理科",$str);
+// echo $str;
+print_r($str);
+
+
---
+++ test9_9/str_replace.php
@@ -0,0 +1,10 @@
+<?php
+
+$str=array("banana","grape");
+print_r($str);
+echo "<br>";
+$str=str_replace("banana","apple",$str);
+// echo $str;
+print_r($str);
+
+
